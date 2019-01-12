666f84511ba65079833fdc9ac114d5d0b58dfc9b
“leonzlotnik” <zlotnik.leon91@gmail.com>
2019-01-12 14:33:20 -0600

    primero

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,50 @@ router.get('/libros', (req, res)=>{
     })
 })
 
+router.get('/libros/add',(req,res)=>{
+  res.render('libros-nuevo');
+});
+
+router.post('libros/add',(res,req) => {
+  const {title, author, description, rating} = req.body;
+  const newBook = new Books({title,author,description,rating});
+  newBook.save()
+  .then((book) => {
+    res.redirect(301,'/libros');
+})
+  .catch((err)=>{
+    console.log(err);
+  })
+});
+
+router.get('libros/edit',(req,res) => {
+ /* const Books = new Books({title, author, description, rating});
+  Books.findOne(({_id: req.query.book_id}))
+  .then((libro)=>{
+    console.log(libro);
+    res.render('edita-libro', {libro})
+  })
+   .catch((err)=>{
+     console.log(err);
+   })*/
+   console.log('Edita libros')
+   res.render('edita-libro');
+});
+
+router.post('libros/edit',(res,req) => {
+  /*const {title, author, description, rating} = req.body;
+  const Books = new Books ({title, author, description, rating})
+  Books.updateOne({_id:req.query.book_id},{$set:{title,autor,descriptionrating}})
+  .then((book) =>{
+    res.redirect('libro')
+  })
+  .catch((err)=>{
+    console.log(err);
+  })*/
+})
+
 router.get('/libros/:id', (req, res)=>{
-  let libroId = req.params.id
+  /*let libroId = req.params.id
   console.log(libroId);
   Books.findOne({'_id': libroId})
   .then((libro)=>{
@@ -27,7 +69,7 @@ router.get('/libros/:id', (req, res)=>{
   })
   .catch((err)=>{
     console.log(err);
-  })
+  })*/
 })
 
 router.post('/buscar', (req, res)=>{
@@ -40,4 +82,21 @@ router.post('/buscar', (req, res)=>{
     console.log(err);
   })
 })
+
+router.get('/autores/nuevos', (res,req) =>{
+  res.render('author-add')
+})
+
+router.post('/author/nuevo',(res,req) =>{
+  const {name,lastName,nationality,birthday,pictureUrl} = req.body
+  const newAuthor = new Author ({name, lastName, nationality, birthday, pictureUrl})
+  newAuthor.save()
+  .then((autor)=>{
+    res.redirect('/libros')
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+})
+
 module.exports = router;
--- seeds2.js
+++ seeds2.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Book = require('../models/book');
-const Author = require('./models/author');
+const Book = require('./models/books');
+const Author = require('./models/authors');
 
 const dbtitle = 'library-project';
 mongoose.connect(`mongodb://localhost/${dbtitle}`);
@@ -159,6 +159,6 @@ const saveBooks = findAuthors.then(findAuthors => {
 }).then(savedBooks => {
   Promise.all(savedBooks)
   .then(books => books.forEach(book => console.log(`created ${book.title}`)))
-  .then(mongoose.connection.close())
+  //.then(mongoose.connection.close())
   .catch(err => console.log("papu",err))
 })
